6239a676b33b381b2d043de02caf4443e41e248e
ewemormartin <martinewemor@gmail.com>
2024-03-14 11:02:59 +0100

    kettes_kesz

--- index.js
+++ index.js
@@ -22,14 +22,36 @@ ELSO.innerHTML=letrehozTablazat();
 
 
 // 2. feladat
+const MASODIK = document.querySelector("#feladat_2")
 function osszesit() {
-    
-
+    let maraton= 0;
+    let felmaraton = 0;
+    let TIZkm = 0;
+    let txt = ""
+    for (let index = 0; index < FUTOK.length; index++) {
+        if (FUTOK[index].versenySzam === "maraton") {
+            maraton++;
+        }
+        else if (FUTOK[index].versenySzam === "félmaraton") {
+            felmaraton++;
+        }
+        else if (FUTOK[index].versenySzam === "10 km") {
+            TIZkm++;
+        }
+    }
+    txt +=  `<p>Maraton: ${maraton}</p>
+             <p>Félmaraton: ${felmaraton}</p>
+             <p>10km: ${TIZkm}</p>`  
+    return txt;
 }
+MASODIK.innerHTML=osszesit();
 
 // 3. feladat
 function befutott(sor, adatok) {
-
+    for (let index = 0; index < FUTOK.length; index++) {
+        
+        
+    }
     
 }
 
