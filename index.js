7b5f0e97e1cb60f9a4746cb574f8f6b07644038d
ewemormartin <martinewemor@gmail.com>
2024-03-14 10:31:17 +0100

    egyes_feladat_kesz

---
+++ index.js
@@ -0,0 +1,43 @@
+import { FUTOK } from "./adatok.js";
+
+const befutottVersenyzok = [];
+
+// 1. feladat
+const ELSO = document.querySelector("feladat_1")
+function letrehozTablazat() {
+    let txt = "<table>"
+    for (let index = 0; index < FUTOK.length; index++) {
+        txt += `<tr>
+                    <td>${FUTOK[index].nev}</td>
+                    <td>${FUTOK[index].nemzetiseg}</td>
+                    <td>${FUTOK[index].versenySzam}</td>
+                </tr>`;
+    }
+    txt += "</table>"
+    ELSO.innerHTML = txt;
+}
+
+
+
+
+// 2. feladat
+function osszesit() {
+
+
+}
+
+// 3. feladat
+function befutott(sor, adatok) {
+
+    
+}
+
+// 4. feladat
+function torolEsemeny() {
+
+}
+
+// 5. feladat
+function ellenoriz(){
+  
+}
